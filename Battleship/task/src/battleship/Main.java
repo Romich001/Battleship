abf3344259a1ea518ceda7335e604f54c4bad7bd
Romich001 <romavoytyuk@gmail.com>
2021-12-13 20:46:41 +0300

    code

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -3,26 +3,28 @@ package battleship;
 public class Main {
 
     public static void main(String[] args) {
-
-        Field player1 = new Field("Player1");
-        Field player2 = new Field("Player2");
-        String winner;
+        //creates two fields for game
+        Field player1 = new Field("Player 1");
+        Field player2 = new Field("Player 2");
+        //game control
         Game game = new Game();
+        //set ships
         game.prepareField(player1);
         game.prepareField(player2);
-        Field shooting = player1;
-        Field shot = player2;
+
         while(true) {
-             game.move(shooting, shot);
-            //System.out.println(shot.getName() + " " + shot.getShipNum());
-             if(player1.getShipNum() == 0 || player2.getShipNum() == 0) break;
+             game.move(player1, player2);
+             if(player1.getShipNum() == 0 || player2.getShipNum() == 0) break; //check if one of the players lost all his ships
              game.passTurn();
-             Field temp = shooting;
-             shooting = shot;
-             shot = temp;
+             //swap the referrals to field
+             Field temp = player1;
+             player1 = player2;
+             player2 = temp;
         }
         game.closeScanner();
         System.out.println("\nYou sank the last ship. You won. Congratulations!");
+
+
 
     }
 
